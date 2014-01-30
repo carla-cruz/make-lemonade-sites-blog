92fca7bd8ba955668a2e100096df6759b2a287a7
Paulette Luftig <pyluftig@gmail.com>
2014-01-29 22:15:44 -0800

    changes to nav for signed in user

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,12 @@
 class StaticPagesController < ApplicationController
 	
-	http_basic_authenticate_with :name => "lemonadeBlog", :password => "weRockEvenMore"
-
 	def blog_admin
 		@posts = Post.all
 		p "*" * 50
 		p @posts
 		p "*" * 50
 		
-@category = Category.new
-	end
+    @category = Category.new
+  end
 
 end
