5e10be386f20ad1d02cc693364f1190e88331de7
Paulette Luftig <pyluftig@gmail.com>
2014-01-30 20:03:02 -0800

    change nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 module ApplicationHelper
-  def cp(path)
-    "active" if current_page?(path)
-  end
-  
   def administrator?(user)
     user.email == "pyluftig@gmail.com" || user.email == "carla13cruz@gmail.com"
   end
