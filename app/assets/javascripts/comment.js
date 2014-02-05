a1dd121660c8e5a3ceb74d7ae62650f677db3a3d
Paulette Luftig <pyluftig@gmail.com>
2014-02-04 20:59:28 -0800

    remove old files

--- app/assets/javascripts/comment.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready ->
-  $(".comment_form").on("ajax:success", (e, data, status, xhr) ->
-    $(".comment_list").append xhr.responseText
-  ).bind "ajax:error", (e, xhr, status, error) ->
-    $("#new_post").append "<p>ERROR</p>"
-    )};
