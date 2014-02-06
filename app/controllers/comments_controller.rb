2aceb93aa9553a908a825398d6793cf435f0a3a9
Paulette Luftig <pyluftig@gmail.com>
2014-02-05 21:57:07 -0800

    add gravatar images

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,12 +5,9 @@ class CommentsController < ApplicationController
   end
 
   def create
-    p "yup here that's for sure"
     @post = Post.find(params[:post_id])
-    p "yup here TOO that's for sure"
     @comment = @post.comments.create!(comment_params)
     respond_to do |format|
-      p "yup here AND IN THE RESPOND TO"
       format.html { redirect_to @post }
       format.js
     end
